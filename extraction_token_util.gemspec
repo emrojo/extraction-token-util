77600e6ce6878d7512b04677a3d2e216d732b8f7
Eduardo Martin Rojo <emr@sanger.ac.uk>
2020-02-25 22:53:09 +0000

    Rubocop

--- extraction_token_util.gemspec
+++ extraction_token_util.gemspec
@@ -1,13 +1,21 @@
+# frozen_string_literal: true
+
 Gem::Specification.new do |s|
-  s.name = %q{extraction_token_util}
-  s.version = "0.0.2"
-  s.version = "#{s.version}-alpha-#{ENV['TRAVIS_BUILD_NUMBER']}" if ENV['TRAVIS']
-  s.date = %q{2020-02-22}
-  s.summary = %q{Set of Ruby methods to provide lexical support to common formats in use in Extraction Lims}
-  s.files = Dir["{lib}/**/*.rb", "bin/*", "LICENSE", "*.md"]
-  s.authors = ["Eduardo Martin Rojo"]
-  s.require_paths = ["lib"]
-  s.license = "MIT"
-  s.homepage = "https://rubygems.org/gems/extraction_token_util"
-  s.add_development_dependency "rspec", '~> 3'
+  s.name = 'extraction_token_util'
+  version = '0.0.3a'
+  version = (ENV['TRAVIS_BUILD_NUMBER']).to_s if version.end_with?('a')
+  s.version = version
+  s.date = '2020-02-22'
+  s.summary = %(
+    Set of Ruby methods to provide lexical support to common formats in use
+    in Extraction Lims
+  )
+  s.files = Dir['{lib}/**/*.rb', 'bin/*', 'LICENSE', '*.md']
+  s.authors = ['Eduardo Martin Rojo']
+  s.require_paths = ['lib']
+  s.license = 'MIT'
+  s.homepage = 'https://rubygems.org/gems/extraction_token_util'
+  s.add_development_dependency 'rspec', '~> 3'
+  s.add_development_dependency 'rubocop', '~> 0.80'
+  s.add_development_dependency 'rubocop-rspec'
 end
--- lib/extraction_token_util.rb
+++ lib/extraction_token_util.rb
@@ -1,85 +1,106 @@
+# frozen_string_literal: true
+
+#
+# This Gem provides support for common string operations with tokens in
+# Extraction lims like:
+# - quoting / unquoting strings
+# - padding / unpadding a string of digits until reaching a fixed size by
+# using zeroes support to perform recognizion of the following tokens:
+# - generate the address positions of the wells of a rack (A01, B01... F12)
+#
+# Also provides pattern recognision for UUID, Wildcard variables (Eg: ?a,
+# ?rack, etc.), Fluidx barcodes: (Eg: FR1234678), Well Location: (Eg: A02, F11)
+#
 module ExtractionTokenUtil
-  def self.UUID_REGEXP
-    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
-  end
+  UUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.freeze
+  WILDCARD = /\?\w*/.freeze
+  LOCATION = /^([A-H])(\d{1,2})$/.freeze
 
   def self.fluidx_barcode_prefix
     'F'
   end
 
-  def self.WILDCARD_REGEXP
-    /\?\w*/
-  end
-
-  def self.LOCATION_REGEXP
-    /^([A-H])(\d{1,2})$/
-  end
-
-  def self.is_uuid?(str)
-    str.kind_of?(String) && !str.match(ExtractionTokenUtil.UUID_REGEXP).nil?
+  def self.uuid?(str)
+    str.is_a?(String) && !str.match(ExtractionTokenUtil::UUID).nil?
   end
 
   def self.quote_if_uuid(str)
-    return quote(str) if is_uuid?(str)
-    return str
+    return quote(str) if uuid?(str)
+
+    str
   end
 
-  def self.is_valid_fluidx_barcode?(barcode)
+  def self.valid_fluidx_barcode?(barcode)
     barcode.to_s.start_with?(fluidx_barcode_prefix)
   end
 
   def self.uuid(str)
-    str.match(ExtractionTokenUtil.UUID_REGEXP)[0]
+    str.match(ExtractionTokenUtil::UUID)[0]
   end
 
-  def self.is_wildcard?(str)
-    str.kind_of?(String) && !str.match(ExtractionTokenUtil.WILDCARD_REGEXP).nil?
+  def self.wildcard?(str)
+    str.is_a?(String) && !str.match(ExtractionTokenUtil::WILDCARD).nil?
   end
 
   def self.kind_of_asset_id?(str)
-    !!(str.kind_of?(String) && (is_wildcard?(str) || is_uuid?(str)))
+    (str.is_a?(String) && (wildcard?(str) || uuid?(str)))
   end
 
   def self.to_asset_group_name(wildcard)
     return wildcard if wildcard.nil?
+
     wildcard.gsub('?', '')
   end
 
   def self.generate_positions(letters, columns)
-    size=letters.size * columns.size
-    location_for_position = size.times.map do |i|
-      "#{letters[(i%letters.length).floor]}#{pad((columns[(i/letters.length).floor]).to_s,'0',2)}"
+    size = letters.size * columns.size
+    size.times.map do |idx|
+      letter = position_letter_for_index(idx, letters)
+      number = position_number_for_index(idx, letters, columns)
+      "#{letter}#{number}"
     end
   end
 
-  def self.pad(str,chr,size)
-    "#{(size-str.size).times.map{chr}.join('')}#{str}"
+  def self.position_letter_for_index(idx, letters)
+    letters[(idx % letters.length).floor]
+  end
+
+  def self.position_number_for_index(idx, letters, columns)
+    pad((columns[(idx / letters.length).floor]).to_s, '0', 2)
+  end
+
+  def self.pad(str, chr, size)
+    "#{(size - str.size).times.map { chr }.join('')}#{str}"
   end
 
   def self.unpad_location(location)
     return location unless location
+
     loc = location.match(/(\w)(0*)(\d*)/)
-    loc[1]+loc[3]
+    loc[1] + loc[3]
   end
 
   def self.pad_location(location)
     return location unless location
-    parts = location.match(ExtractionTokenUtil.LOCATION_REGEXP)
-    return nil if parts.length == 0
+
+    parts = location.match(ExtractionTokenUtil::LOCATION)
+    return nil if parts.to_a.empty?
+
     letter = parts[1]
     number = parts[2]
-    number = ExtractionTokenUtil.pad(number,"0",2) unless number.length == 2
+    number = ExtractionTokenUtil.pad(number, '0', 2) unless number.length == 2
     "#{letter}#{number}"
   end
 
   def self.quote(str)
     return str unless str
+
     "\"#{str}\""
   end
 
   def self.unquote(str)
     return str unless str
-    str.gsub(/\"/,"")
-  end
 
+    str.gsub(/\"/, '')
+  end
 end
